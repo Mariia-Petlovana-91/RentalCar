4f72f2e6535c57c17aaf1c80beb9f0cd7074dca8
Mariia <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2025-02-24 23:58:55 +0200

    create thanck

---
+++ src/redux/cars/operation.js
@@ -0,0 +1,78 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+import toast from 'react-hot-toast';
+
+import {
+  getCars,
+  getCarById,
+  getBrands,
+} from '../../api/api';
+
+export const getCarsThunk =
+  createAsyncThunk(
+    'cars/getCars',
+    async (
+      params,
+      { rejectWithValue },
+    ) => {
+      try {
+        const data =
+          await getCars(
+            params,
+          );
+        return data;
+      } catch (error) {
+        toast.error(
+          'Upss not found cars.😔 Try again later.',
+        );
+        return rejectWithValue(
+          error.message,
+        );
+      }
+    },
+  );
+
+export const getCarByIdThank =
+  createAsyncThunk(
+    'cars/getCarsById',
+    async (
+      id,
+      { rejectWithValue },
+    ) => {
+      try {
+        const data =
+          await getCarById(
+            id,
+          );
+        return data;
+      } catch (error) {
+        toast.error(
+          'Upss not found car.😔 Try again later.',
+        );
+        return rejectWithValue(
+          error.message,
+        );
+      }
+    },
+  );
+
+export const getBrandsThank =
+  createAsyncThunk(
+    'brands/getBrands',
+    async ({
+      rejectWithValue,
+    }) => {
+      try {
+        const array =
+          await getBrands();
+        return array;
+      } catch (error) {
+        toast.error(
+          'Upss not found brands.😔 Try again later.',
+        );
+        return rejectWithValue(
+          error.message,
+        );
+      }
+    },
+  );
