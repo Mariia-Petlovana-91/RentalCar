da1a1c3379f521e3db22552e6030340044f47aff
Mariia <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2025-03-02 15:27:26 +0200

    add notFoundPagr

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,12 +1,11 @@
 import css from '../NotFound/NotFound.module.css';
 
 import Section from '../../components/base/Section/Section';
-import Container from '../../components/base/Container/Container';
 
 const NotFound = () => {
   return (
     <Section>
-      <Container></Container>
+      <div className={css.notFound}></div>
     </Section>
   );
 };
