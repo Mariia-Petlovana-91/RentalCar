cf5f2314471ebab73ed78f8b275539548a46a8e9
Mariia <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2025-03-03 08:25:09 +0200

    fix notFoundPage

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,11 +1,15 @@
 import css from '../NotFound/NotFound.module.css';
+import clsx from 'clsx';
 
 import Section from '../../components/base/Section/Section';
 
 const NotFound = () => {
   return (
     <Section>
-      <div className={css.notFound}></div>
+      <h2 className={clsx('secondaryTitle', css.notFoundTitle)}>
+        Page not found
+      </h2>
+      <div className={css.notFoundImg}></div>
     </Section>
   );
 };
