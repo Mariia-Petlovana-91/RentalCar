e2d0761ea04e6584fc9ccef905d5de0c31ea3130
Mariia <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2025-03-07 02:54:34 +0200

    fix height  nothingItem

--- src/components/NotItem/NotItem.jsx
+++ src/components/NotItem/NotItem.jsx
@@ -1,6 +1,8 @@
+import clsx from 'clsx';
+
 const NotItem = () => {
   return (
-    <div className="nothingContainer">
+    <div className={clsx('nothingContainer', 'nothingContainerItem')}>
       <p className="nothingText">
         You have reached the end of the list.
       </p>
